324b2177af075a46820047efa3e7528658dedeae
sierchio <jensierchio@gmail.com>
2017-07-29 19:21:36 -0400

    Add files via upload

--- plot1.R
+++ plot1.R
@@ -4,10 +4,10 @@ setwd("/users/jen/R_Programming")
 fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 download.file(fileUrl, "electricpower.zip", method = "curl")
 unzip("electricpower.zip")
-full_data <- read.delim("household_power_consumption.txt",header=TRUE,sep=";")
+full_data <- read.delim("household_power_consumption.txt",header=TRUE,sep=";",stringsAsFactors=FALSE)
 
 ## we need to extract the data between Feb 1 2007 and Feb 2 2007 as directed in the instructions
-sub_data = full_data[full_data$Date %in% c("1/2/2007","2/2/2007") ,]
+sub_data <- full_data[full_data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 ## We can only create a histogram on numeric data
 globalactivepower <- as.numeric(sub_data$Global_active_power)
@@ -16,7 +16,7 @@ globalactivepower <- as.numeric(sub_data$Global_active_power)
 png("plot1.png", width=480, height=480)
 
 ## We need the bars to be red, the title and the x label
-hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+hist(globalactivepower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 
 ## turn png device off
 dev.off()
